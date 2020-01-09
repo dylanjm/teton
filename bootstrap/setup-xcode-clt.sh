e8a1db4e5b664cc9ca1d7f888d0174541ffc5cf2
Dylan McDowell <dylanjm@users.noreply.github.com>
2020-01-09 13:54:56 -0700

    modify github workflows

--- bootstrap/setup-xcode-clt.sh
+++ bootstrap/setup-xcode-clt.sh
@@ -37,4 +37,19 @@ function bootstrap-xcode-clt () {
   fi
 }
 
+function install-clt () {
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+
+  PROD=$(softwareupdate -l |
+           grep "\*.*Command Line" |
+           head -n 1 |
+           awk -F"*" '{print $2}' |
+           sed -e 's/^ *//' |
+           tr -d '\n')
+
+  softwareupdate -i "$PROD" --verbose
+
+  rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+}
+
 bootstrap-xcode-clt

b23c1272ad03b7c5a0f70c4b9472c630e3df7daa
Dylan McDowell <dylanjm@users.noreply.github.com>
2023-01-23 13:31:34 -0700

    Switch to Corfu & Emacs 29

--- config/shells/zsh/interactive/aliases.zsh
+++ config/shells/zsh/interactive/aliases.zsh
@@ -23,7 +23,7 @@ autoload -Uz  run-help    run-help-git  run-help-ip   run-help-openssl \
 alias tconf="${EDITOR} ${DOTFILES}"
 alias zconf="${EDITOR} ${ZSH}"
 alias econf="${EDITOR} ${DOTFILES}/config/emacs"
-alias kemc="brew services restart emacs-head@28"
+alias kemc="brew services restart emacs-head@29"
 alias em="/usr/local/bin/emacsclient -t -c -a ''"
 
 ###
